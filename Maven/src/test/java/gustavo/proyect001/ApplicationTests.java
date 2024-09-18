33123b8b8fd94446a26b19fad33b6bac686d5e0d
Ripper2021 <95532358+Ripper2021@users.noreply.github.com>
2024-09-18 07:42:36 -0300

    Add files via upload

---
+++ Maven/src/test/java/gustavo/proyect001/ApplicationTests.java
@@ -0,0 +1,13 @@
+package gustavo.proyect001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
